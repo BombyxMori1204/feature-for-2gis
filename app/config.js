8a79ab3dfee845904caea22574fadefa428b018e
BombyxMori1204 <alexzinovev05@gmail.com>
2025-10-05 11:46:56 +0300

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 // В реальном проде ключ лучше проксировать через бэкенд (см. server.js ниже).
-export const DGIS_KEY = "7ae4c441-8a0a-42a4-b0a6-e88b9c23c6b5";
+export const DGIS_KEY = "api_key";
 
 // Базовый URL Suggest API (2ГИС Catalog v3)
 export const SUGGEST_URL = "https://catalog.api.2gis.com/3.0/suggests";
+
 
