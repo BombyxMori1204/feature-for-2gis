e24b8695595ac6c7e763f757272cb801223bd6e4
BombyxMori1204 <alexzinovev05@gmail.com>
2025-10-05 11:52:31 +0300

    Delete app/config.js

--- app/config.js
+++
@@ -1,7 +0,0 @@
-// В реальном проде ключ лучше проксировать через бэкенд (см. server.js ниже).
-export const DGIS_KEY = "api_key";
-
-// Базовый URL Suggest API (2ГИС Catalog v3)
-export const SUGGEST_URL = "https://catalog.api.2gis.com/3.0/suggests";
-
-
